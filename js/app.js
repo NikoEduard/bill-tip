86952a43f104b47b7fb020f3894a26b1e2233166
Eduard <106318374+NikoEduard@users.noreply.github.com>
2023-08-07 11:52:15 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const percentBtns = document.querySelectorAll(".percent-btns");
 const customPercent = document.getElementById("custom__percent");
 const numberOfPeople = document.querySelector("#people__amount").value;
 let resultTipsPerPerson =
-  document.querySelector(".result__tips-info").textContent;
+  document.querySelector(".result__tips-info");
 let resultBillPerPerson =
   document.querySelector(".result__bill-info");
 const getResultBtn = document.querySelector(".result__btn");
