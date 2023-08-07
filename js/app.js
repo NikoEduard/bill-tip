4f92acfe267b4b88d2cb53ecc8338cbfd0821dd5
Eduard <106318374+NikoEduard@users.noreply.github.com>
2023-08-07 09:16:21 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,13 +8,13 @@ const customPercent = document.getElementById("custom__percent");
 const numberOfPeople = document.querySelector("#people__amount").value;
 let resultTipsPerPerson =
   document.querySelector(".result__tips-info").textContent;
-let resultBillPerPerson =
-  document.querySelector(".result__bill-info").textContent;
+let resultBillPerPerson =Number(
+  document.querySelector(".result__bill-info").textContent);
 const getResultBtn = document.querySelector(".result__btn");
 const resetBtn = document.querySelector(".result__reset");
 
 function getBillForOnePerson() {
-return resultBillPerPerson.textContent = Number(billAmount) / Number(numberOfPeople);
+return resultBillPerPerson = Number(billAmount) / Number(numberOfPeople);
 }
 getResultBtn.addEventListener("click", function() {
   getBillForOnePerson();
